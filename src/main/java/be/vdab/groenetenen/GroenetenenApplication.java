21fb0c711726e2664053d77860eb32e73969ae89
Stijn Verholen <Stijn.Verholen@ad.core.vdabantwerpen.intra>
2018-10-19 11:10:29 +0200

    Initial Spring Boot project

---
+++ src/main/java/be/vdab/groenetenen/GroenetenenApplication.java
@@ -0,0 +1,12 @@
+package be.vdab.groenetenen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroenetenenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroenetenenApplication.class, args);
+	}
+}
