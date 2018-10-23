6f400bd2d01a06c49fdbd82c97cedbbd29746d61
sverholen <sverholen@gmail.com>
2018-10-23 21:38:15 +0200

    Cursus klaar

--- src/main/java/be/vdab/groenetenen/security/SecurityConfig.java
+++ src/main/java/be/vdab/groenetenen/security/SecurityConfig.java
@@ -66,7 +66,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.mvcMatchers("/tenders/add").hasAuthority(MANAGER)
 			.mvcMatchers("/employees")
 				.hasAnyAuthority(STOCKKEEPER, HELPDESK)
-			.mvcMatchers("/", "/branches", "/login").permitAll()
+			.mvcMatchers("/", "/branches/**", "/login").permitAll()
 			.mvcMatchers("/**").authenticated();
 		
 		http.httpBasic();
